55bcef52d03d365f595c6c25a603764e7e3b517c
Smoren <ofigate@gmail.com>
2023-11-20 18:07:31 +0300

    fix

--- solib/test_distance.py
+++ solib/test_distance.py
@@ -11,7 +11,7 @@ distlib.l2distance.argtypes = [c_double, c_double, c_double, c_double]
 distlib.l2distance.restype = c_double
 
 
-def py_l2distance(x1, x2, y1, y2):
+def py_l2distance(x1, y1, x2, y2):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 
